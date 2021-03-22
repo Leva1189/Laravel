5bfa56ed23ea877a73ff5f79b474ae4b665b0e8f
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-03-22 14:51:43 +0200

    Laravel5_lesson_migrate

--- laravel5/src/app/Http/routes.php
+++ laravel5/src/app/Http/routes.php
@@ -2,15 +2,44 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', ['as'=>'home','uses'=>'Admin\IndexController@show']);
+
+Route::get('/about',['uses'=>'Admin\AboutController@show','as'=>'about']);
+
+
+Route::get('/articles',['uses'=>'Admin\Core@getArticles','as'=>'articles']);
+
+
+Route::get('/article/{id}',['uses'=>'Admin\Core@getArticle','as'=>'article']);
+
+Route::match(['get','post'],'/contact/{name?}',['uses'=>'Admin\ContactController@show','as'=>'contact']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+    ////
+
+
+    ///
+
 });
